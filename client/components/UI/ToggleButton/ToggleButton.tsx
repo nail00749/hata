5861cb9736b455925b4547a729f8d22936580162
nail- <nail00749@gmail.com>
2022-10-02 17:31:58 +0500

    add to toggle button orientation

--- client/components/UI/ToggleButton/ToggleButton.tsx
+++ client/components/UI/ToggleButton/ToggleButton.tsx
@@ -1,13 +1,22 @@
 import { ButtonHTMLAttributes, FC, ReactNode, MouseEvent } from 'react';
+import { OrientationType } from '../../../models/UI/Orientation';
 
 export interface ToggleButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
   value?: any;
   active?: boolean;
   setValue?: (value: any, e: MouseEvent<HTMLButtonElement>) => void;
+  orientation?: OrientationType;
 }
 
-export const ToggleButton: FC<ToggleButtonProps> = ({ children, active, value, setValue, ...otherProps }) => {
+export const ToggleButton: FC<ToggleButtonProps> = ({
+                                                      children,
+                                                      active,
+                                                      value,
+                                                      setValue,
+                                                      orientation='horizontal',
+                                                      ...otherProps
+                                                    }) => {
   const handlerClick = (val: any) => (e: MouseEvent<HTMLButtonElement>) => {
     if (setValue) {
       setValue(val, e);
@@ -16,11 +25,13 @@ export const ToggleButton: FC<ToggleButtonProps> = ({ children, active, value, s
 
   return (
     <button
+      //
       className = {`
         px-3 py-2
         border-solid border-2 border-sky-500
-        first:rounded-l-2xl last:rounded-r-2xl
-        first:border-r-0 last:border-l-0
+        ${orientation === 'horizontal' ? 'first:border-l-2 last:border-l-0 border-l-0 border-r-1' : 'first:border-b-0 last:border-b-2 border-t-1 border-b-0'}
+        ${orientation === 'horizontal' ? 'first:rounded-l-xl last:rounded-r-xl' : 'first:rounded-t-xl last:rounded-b-xl'}
+        
         ${active ? 'bg-sky-200' : ''}
       `}
       {...otherProps}
--- client/components/UI/ToggleButton/ToggleButtonGroup.tsx
+++ client/components/UI/ToggleButton/ToggleButtonGroup.tsx
@@ -1,23 +1,30 @@
 import React, { Children, cloneElement, FC, ReactElement, MouseEvent } from 'react';
 import { ToggleButton, ToggleButtonProps } from './ToggleButton';
+import { OrientationType } from '../../../models/UI/Orientation';
 
 
 interface ToggleButtonGroupProps {
   children?: ReactElement<ToggleButtonProps> | Array<ReactElement<ToggleButtonProps>>;
   value?: any;
   onClick?: (val: any, e: MouseEvent<HTMLButtonElement>) => void;
+  orientation?: OrientationType
 }
 
-export const ToggleButtonGroup: FC<ToggleButtonGroupProps> = ({ children, value, onClick }) => {
+export const ToggleButtonGroup: FC<ToggleButtonGroupProps> = ({ children, value, onClick, orientation = 'horizontal'  }) => {
   return (
     <div>
-      {Children.map(children, (child) => {
-        if (child?.type != ToggleButton) {
-          return;
+      тип жилья
+      <div
+        className={`flex ${orientation === 'veritical' ? 'flex-col' : ''}`}
+      >
+        {Children.map(children, (child) => {
+          if (child?.type != ToggleButton) {
+            return;
+          }
+          return cloneElement({ ...child }, { active: value === child.props.value, setValue: onClick, orientation });
+        })
         }
-        return cloneElement({ ...child }, { active: value === child.props.value, setValue: onClick });
-      })
-      }
+      </div>
     </div>
   );
 };
